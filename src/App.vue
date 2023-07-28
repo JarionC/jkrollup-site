193da882e6738a2c7147bda7d303d5991e01088d
JarionC <130309514+JarionC@users.noreply.github.com>
2023-07-27 21:10:58 -0400

    indev-update

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,26 @@ export default {
 </script>
 
 <style>
+html{
+  height:100%;
+  width:100%;
+}
+body{
+      width:100%;
+      height:100%;
+      background-color: black;
+      box-sizing: border-box;
+      padding:0;
+      margin: 0;
+    }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width:100%;
+      height:100%;
+      box-sizing: border-box;
 }
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="home-container">
       <div class="home-title">
-        JK-Rollup
-      </div>
-      <div class="home-subtitle">
+        <div class="title-img">
 
+        </div>
+        <div class="title-text">
+          Rollup
+        </div>
       </div>
+      
+      
+    <div class="dev-text">
+      In Development
+    </div>
     </div>
   </template>
   
@@ -27,10 +34,96 @@
       src: 
         url('../fonts/Exo.ttf');
     }
+
+    @font-face{
+      font-family: "MangaBey";
+      src: 
+        url('../fonts/mangabey.otf');
+    }
+    .home-container{
+      width:100%;
+      box-sizing: border-box;
+      padding-top:100px;
+      display:flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .dev-text {
+      margin-top: 100px;
+      color:white;
+      font-family: 'Exo';
+      font-size: 1em;
+      font-weight: bold;
+      opacity: 0;
+      animation: imgFadeIn 2s ease-in forwards, textglow 5s infinite;
+      animation-delay: 5s, 7s;
+    }
     .home-title{
+      display : flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      box-sizing: border-box;
+      margin-bottom:100px;
+      flex-wrap: nowrap;
+    }
+    .title-img{
+      background-image: url('../img/jk_logo_white.png');
+      background-repeat: no-repeat;
+      background-size: contain;
+      margin-left: 10px;
+      width: 10em;
+      height: 10em;
+      opacity: 0;
+      animation: imgFadeIn 2s ease-in forwards
+    }
+    .title-text{
+      display:flex;
       font-size:4em;
       font-weight:bold;
       font-family: 'Exo';
+      color:transparent;
+      text-align: center;
+      justify-content: center;
+      align-items: center;
+      opacity: 0;
+      animation: imgFadeIn 3s ease-in forwards;
+      
+      -webkit-text-stroke: .03em white;
+      animation-delay: 2s;
+    }
+
+    @keyframes textglow {
+      0% { -webkit-filter: none; }
+      50% { -webkit-filter: drop-shadow(0 0 20px #fff) drop-shadow(0 0 50px #fff) }
+      100% { -webkit-filter: none; }
+    }
+    @keyframes imgFadeIn {
+      to {
+        opacity: 1;
+      }
+    }
+
+    @keyframes textFadeIn {
+      to {
+        opacity: 1;
+      }
+    }
+    .home-subtitle{
+      font-size: 2em;
+      margin-bottom: 50px;
+      color:white;
+    }
+    .ats-image{
+      
+      background-image: url('../img/trade_logo_white.png');
+      background-repeat: no-repeat;
+      background-size: contain;
+      margin-left: 10px;
+      width: 200px;
+      height: 180px;
     }
   </style>
   
